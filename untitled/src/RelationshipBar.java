ea2bf120fb1885071af1abc04d97503e2d508b8e
zoya <zdzhnou@gmail.com>
2024-12-03 18:49:54 -0800

    added Flag impacting relationship bar + toString methods

--- untitled/src/RelationshipBar.java
+++ untitled/src/RelationshipBar.java
@@ -1,12 +1,50 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class RelationshipBar {
     private double currentStatus;
+    private Queue<Flag> flagHistory;
 
     public RelationshipBar() {
         currentStatus = 50;
     }
     public RelationshipBar(double initialStatus) {
         currentStatus = initialStatus;
+        flagHistory = new LinkedList<>();
     }
+
+    public void addFlagImpact(Flag coloredFlag) {
+        flagHistory.add(coloredFlag);
+        currentStatus += coloredFlag.getMagnitude();
+    }
+
+    public String statusToString() {
+        String retString = "Current Status: ";
+        int numBars = (int) currentStatus / 5;
+        for (int i = 0; i < numBars; i++) {
+            retString += "[] ";
+        }
+        for (int i = numBars; i < 20; i++) {
+            retString += "__ ";
+        }
+        retString += "(" + +currentStatus + "/100 ";
+
+        return retString;
+    }
+    public String pastFlagsToString() {
+//        String retString = "";
+//        Iterator iterator = flagHistory.iterator();
+
+        return flagHistory.toString();
+    }
+
+    public String toString() {
+        return statusToString() + "\n\nFlag history: " + pastFlagsToString();
+    }
+
+
+
 
 
 }
