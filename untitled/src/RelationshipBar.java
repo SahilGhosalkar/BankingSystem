ab7a1c89a26e8467de7c1fe4216b8f6b77339a5c
zoya <zdzhnou@gmail.com>
2024-12-03 18:20:50 -0800

    constructors

--- untitled/src/RelationshipBar.java
+++ untitled/src/RelationshipBar.java
@@ -1,2 +1,12 @@
 public class RelationshipBar {
+    private double currentStatus;
+
+    public RelationshipBar() {
+        currentStatus = 50;
+    }
+    public RelationshipBar(double initialStatus) {
+        currentStatus = initialStatus;
+    }
+
+
 }
