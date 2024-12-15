8f8131dfdcbba4edbd1e37c5bbd48ad106973d30
kelleyzha <kzhan129@ucsc.edu>
2024-12-14 18:48:14 -0800

    flagindex class and flagindex tester done

--- src/App/FlagIndex.java
+++ src/App/FlagIndex.java
@@ -1,5 +1,35 @@
 package App;
 
-public class FlagIndex {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Collections;
 
+public class FlagIndex {
+    private Map<Integer, Flag> flagMap;
+
+    public FlagIndex() {
+        //map to store the ID-Flag pairs
+        flagMap = new HashMap<>();
+    }
+
+
+    //Adds a Flag to the index with ID
+    public void addFlag(int id, Flag flag) {
+        flagMap.put(id, flag);
+    }
+
+     //Removes the Flag associated with the given ID.
+    public boolean removeFlag(int id) {
+        return flagMap.remove(id) != null;
+    }
+
+    //Retrieves the Flag associated with the given ID.
+    public Flag getFlag(int id) {
+        return flagMap.get(id);
+    }
+
+    //Returns a read-only view of all Flags stored
+    public Map<Integer, Flag> getAllFlags() {
+        return Collections.unmodifiableMap(flagMap);
+    }
 }
--- src/Testers/FlagTester.java
+++ src/Testers/FlagTester.java
@@ -5,12 +5,12 @@ import App.*;
 
 
 public class FlagTester {
-    public static void main(String[] args) {
-<<<<<<< HEAD:untitled/src/Testers/FlagTester.java
-
-=======
->>>>>>> main:untitled/src/FlagTester.java
-        Flag flag = new RedFlag("Doesnt want to show me off", -20);
-        System.out.println(flag.toString());
-    }
+//    public static void main(String[] args) {
+//<<<<<<< HEAD:untitled/src/Testers/FlagTester.java
+//
+//=======
+//>>>>>>> main:untitled/src/FlagTester.java
+//        Flag flag = new RedFlag("Doesnt want to show me off", -20);
+//        System.out.println(flag.toString());
+//    }
 }
