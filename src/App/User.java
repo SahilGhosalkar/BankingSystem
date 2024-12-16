18606a1e30736b880adaf42f0668f924f6326347
sahil <559553@my.cuhsd.org>
2024-12-15 22:35:50 -0800

    finished user hopefully

--- src/App/User.java
+++ src/App/User.java
@@ -1,5 +1,11 @@
 package App;
 
-public class User {
-    
+public class User extends Person {
+    public User() {
+        super();
+    }
+
+    public User(String name, String eyeColor, int heightInches, String gender, int age) {
+        super(name, eyeColor, heightInches, gender, age);
+    }
 }
